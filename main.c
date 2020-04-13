1a7300ffecb1b3f692ef8b6817bf7d92b00f85c2
Karol Syta <karolsyta7@gmail.com>
2020-04-13 12:15:05 +0200

    Add createDirectories function

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#ifndef _WIN32
+#include <sys/stat.h>
+#endif
+
 #include "login.h"
 #include "albums.h"
 
+void createDirectories()
+{
+#ifdef _WIN32
+  _mkdir("./albums");
+  _mkdir("./users");
+#else
+  mkdir("./albums", 0700);
+  mkdir("./users", 0700);
+#endif
+}
+
 int main()
 {
+  createDirectories();
   bool isLogged;
 
   do
